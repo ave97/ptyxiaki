cd36e0380c640fbd00ab5bccf18276c1cee6ed65
Aventis <aveave97@hotmail.com>
2025-05-13 23:06:23 +0300

    Last version 13.5.v.2

--- app.py
+++ app.py
@@ -905,11 +905,11 @@ def no_access():
     return render_template("no_access.html")
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
 # if __name__ == "__main__":
-#     import os
+#     app.run(debug=True)
 
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
